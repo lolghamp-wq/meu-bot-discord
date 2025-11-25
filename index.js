f83c87f6ec12ef9365d024351e3f96ae6fd6e21a
lolghamp-wq <lolghamp@gmail.com>
2025-11-24 22:06:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+require("dotenv").config();
 
+const { Client, GatewayIntentBits } = require("discord.js");
+const { responderPokemon } = require("./pokedex");
+
+const TOKEN = process.env.TOKEN;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Bot logado como ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("!pkm ")) {
+    const nome = message.content.replace("!pkm ", "").trim();
+
+    const resposta = responderPokemon(nome);
+
+    message.reply(resposta);
+  }
+});
+
+client.login(TOKEN);
